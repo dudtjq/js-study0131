082a588d410efa04e5bcbef3b62ff7f31281a3ce
dudtjq <dudtjq0415@naver.com>
2024-02-19 18:17:22 +0900

    공부하기

--- 2.web-api/App/TodoList/app.js
+++ 2.web-api/App/TodoList/app.js
@@ -2,7 +2,7 @@
 // 일정 데이터가 들어 있는 배열 선언
 const todos = [];
 
-// 화면에 표현할 li.todo-list-item 노드를 생성하는 함수 정의
+// 1 - 1  화면에 표현할 li.todo-list-item 노드를 생성하는 함수 정의
 function makeNewTodoNode(newTodo){
   // 감싸고 있는 태그들을 먼저 생성
   const $li = document.createElement('li');
@@ -12,13 +12,13 @@ function makeNewTodoNode(newTodo){
 
   // lable 태그 작업
   $label.classList.add('checkbox');
-  const $check = document.createElement('input');
-  $check.setAttribute('type', 'checkbox');
-  const $span = document.createElement('span');
-  $span.classList.add('text');
-  $span.textContent = newTodo.text;
-  $label.appendChild($check);
-  $label.appendChild($span);
+  const $check = document.createElement('input');// 인풋태그 만들기
+  $check.setAttribute('type', 'checkbox'); // 인풋태그 타입 설정
+  const $span = document.createElement('span'); // span 만들기
+  $span.classList.add('text'); //span 안에 text 라는 class 만들기 
+  $span.textContent = newTodo.text; // span text안에 넣을 문구 생성 $todoText.value,
+  $label.appendChild($check); //
+  $label.appendChild($span); // 라벨에 추가하기
 
   // modify 태그 작업
   $divMod.classList.add('modify');
@@ -55,7 +55,7 @@ function makeNewTodoNode(newTodo){
 
 
 
-// 추가될 할 일 객체의 id를 생성해 주는 함수 정의
+// 2 - 1  추가될 할 일 객체의 id를 생성해 주는 함수 정의
 function makeNewId(){
   if(todos.length > 0){
     // 배열 내의 할일 객체 중 마지막 객체의 id 보다 하나 크게
@@ -70,7 +70,7 @@ function makeNewId(){
 
 
 
-// 할 일 추가 처리 함수 정의
+// 1. 할 일 추가 처리 함수 정의
 function insertTodoData(){
 
   // 사용자가 작성할 일 일 input 요소 얻기 
@@ -139,7 +139,7 @@ function findIndexByDataId(dataId){
 
 
 
-// 할 일 완료 처리 수행 할 함수 정의
+// 2. 할 일 완료 처리 수행 할 함수 정의
 function changeCheckState($label){
 
   /*
@@ -171,7 +171,7 @@ function changeCheckState($label){
 
 
 
-// 할 일 삭제 처리 함수 정의
+// 3. 할 일 삭제 처리 함수 정의
 function removeTodoData($delTarget){
 
   // 애니메이션 적용을 위해 클래스 이름을 추가(delMoving)
@@ -197,7 +197,7 @@ function removeTodoData($delTarget){
 
 }
 
-// 수정 모드 진입 이벤트 함수
+// 4. 수정 모드 진입 이벤트 함수
 function enterModifyMode($modSpan){
 
   // 수정모드 진입 버튼 교체 (lnr-undo -> lnr-checkmark-circle)
@@ -220,7 +220,7 @@ function enterModifyMode($modSpan){
 
 }
 
-// 수정 완료 이벤트
+// 4. 수정 완료 이벤트
 function modifyTodoData($modCompleteSpan){
 
   //버튼을 원래대로 돌립니다. (lnr-undo)
@@ -233,8 +233,8 @@ function modifyTodoData($modCompleteSpan){
 
   const $textSpan = document.createElement('span');
   $textSpan.textContent = $modInput.value;
-  $textSpan.classList.add('text')
-
+  $textSpan.classList.add('text');
+ //                세롭게 추가   기존 것을 삭제
   $label.replaceChild($textSpan, $modInput);
 
   // 배열 내의 id가 일치하는 객체를 찾아서 text 프로퍼티의 값을 수정된 값으로 변경
@@ -251,7 +251,7 @@ function modifyTodoData($modCompleteSpan){
 // 메인 역할을 하는 즉시 실행 함수.
 (function() {
 
-  // 할 일 추가 이벤트 등록
+  // 1. 할 일 추가 이벤트 등록
   const $addBtn = document.getElementById('add');
 
   $addBtn.addEventListener('click', e => {
@@ -264,7 +264,7 @@ function modifyTodoData($modCompleteSpan){
   });
 
 
-  // 할 일 완료 처리(체크 박스) 이벤트
+  // 2. 할 일 완료 처리(체크 박스) 이벤트
   const $todoList = document.querySelector('.todo-list');
 
   $todoList.addEventListener('click', e => {
@@ -277,7 +277,7 @@ function modifyTodoData($modCompleteSpan){
 
   });
   
-  // 할일 삭제 이벤트
+  // 3. 할일 삭제 이벤트
 
   $todoList.addEventListener('click', e =>{
 
@@ -291,7 +291,7 @@ function modifyTodoData($modCompleteSpan){
   });
 
 
-  // 할 일 수정 이벤트(수정 모드 진입, 수정 완료)
+  // 4. 할 일 수정 이벤트(수정 모드 진입, 수정 완료)
   $todoList.addEventListener('click', e => {
 
     if(e.target.matches('.todo-list .modify span.lnr-undo')){
